70df85788c42ddd41b82d444e9e47c3d60dd9f3c
npc-gnu <root_4rch@proton.me>
2025-08-19 13:32:46 +0000

    animatel became better

--- animatel.cpp
+++ animatel.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-void animatel(char letter, int time_milisecond) {
-    std::cout << letter;
-    std::cout.flush();
-    std::this_thread::sleep_for(std::chrono::milliseconds(time_milisecond));
-}
+#include <atomic>
+#include "animatel.hpp"
+std::atomic<bool> animating(true);
+void animatel(char letter, int time_milisecond){
+    while(animating){
+        std::cout << letter;
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(time_milisecond));
+    }}
